c3e68002de89aefc7dcb2c5771e54be4421d9d1f
AliDeveci <aliefe8082@gmail.com>
2022-03-31 23:17:13 +0300

    work 3 is done and about to be pushed to github after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,4 +5,7 @@ public class HelloWorld {
 
     //Doing awesome work 2 !
 
+    //Doing awesome work 3 !
+    //Avesome work 3 is dome and ready to be committed and ready to be pushed to remote repository
+
 }
